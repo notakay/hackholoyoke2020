f2ebc3763e5f4653539487190868cdc8b2465663
akay <aungkhantko@protonmail.com>
2020-11-08 01:50:35 +0630

    Testing stuff

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 import * as React from 'react';
 import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, {Source, Layer} from 'react-map-gl';
 
 function App() {
   return (
@@ -17,13 +17,33 @@ function App() {
   );
 }
 
+const EMPTY_STYLE = {
+  version: 8,
+  sources: {},
+  layers: []
+};
+
+const geoJSON = {
+  "type": "FeatureCollection",
+  "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+  "features": [
+    { "type": "Feature", "properties": { "id": "ak16994521", "mag": 2.3, "time": 1507425650893, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [-151.5129, 63.1016, 0.0] } },
+    { "type": "Feature", "properties": { "id": "ak16994519", "mag": 1.7, "time": 1507425289659, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [-150.4048, 63.1224, 105.5] } },
+    { "type": "Feature", "properties": { "id": "ak16994517", "mag": 1.6, "time": 1507424832518, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [-151.3597, 63.0781, 0.0] } },
+    { "type": "Feature", "properties": { "id": "ci38021336", "mag": 1.42, "time": 1507423898710, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [-118.497, 34.299667, 7.64] } },
+    { "type": "Feature", "properties": { "id": "us2000b2nn", "mag": 4.2, "time": 1507422626990, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [-87.6901, 12.0623, 46.41] } },
+    { "type": "Feature", "properties": { "id": "ak16994510", "mag": 1.6, "time": 1507422449194, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [-151.5053, 63.0719, 0.0] } },
+    { "type": "Feature", "properties": { "id": "us2000b2nb", "mag": 4.6, "time": 1507420784440, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [-178.4576, -20.2873, 614.26] } }
+  ]
+}
+
 function Map() {
   const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8
+    width: 1200,
+    height: 800,
+    latitude: 0,
+    longitude: -180,
+    zoom: 2
   });
 
   // remove 
@@ -32,7 +52,17 @@ function Map() {
       {...viewport}
       mapboxApiAccessToken="pk.eyJ1Ijoibm90YWtheSIsImEiOiJja2g2a24yamkwZmFwMzB1aWw3OG90NDZmIn0.1IYxmhdVDKYXnQBQZx6vPw"
       onViewportChange={nextViewport => setViewport(nextViewport)}
-    />
+    >
+      <Source id="my-data" type="geojson" data={geoJSON}>
+        <Layer
+          id="point"
+          type="circle"
+          paint={{
+            'circle-radius': 10,
+            'circle-color': '#007cbf'
+          }} />
+      </Source>
+    </ReactMapGL>
   );
 }
 
