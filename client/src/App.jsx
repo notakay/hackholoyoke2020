87555b8db870964354a28592cff124e1cf2d4d58
akay <aungkhantko@protonmail.com>
2020-11-08 14:01:05 +0630

    Added slider + sample data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-// import 'bootstrap/dist/css/bootstrap.min.css'
 import Geo from './containers/geo';
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Geo />
-        {/* <Map/> */}
       </header>
     </div>
   );
--- client/src/containers/geo/index.jsx
+++ client/src/containers/geo/index.jsx
@@ -6,43 +6,40 @@ const Geo = () => {
   const [viewport, setViewport] = useState({
     width: 1200,
     height: 600,
-    latitude: 40.7331,
-    longitude: -73.9712,
-    zoom: 12
+    latitude: 42.3601,
+    longitude: -71.0589,
+    zoom: 8
   });
 
-  const [date, setDate] = useState(0)
+  const minTimeString = "2020-01-01";
+  const maxTimeString = "2020-01-31";
 
+  const minTime = (new Date(minTimeString)).getTime();
+  const maxTime = (new Date(maxTimeString)).getTime();
+  
+  const [date, setDate] = useState(minTime)
+  const [dateString, setDateString] = useState( minTimeString  );
 
-  const EMPTY_STYLE = {
-    version: 8,
-    sources: {},
-    layers: []
-  };
+  const handleInput = (e) => {
+    let d = new Date();
+    d.setTime(e.target.value);
+    setDateString(d.toISOString().substring(0, 10));
+    return setDate(e.target.value);
+  }
 
   const geoJSON = {
-    "type": "FeatureCollection",
-    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "type": "FeatureCollection", 
     "features": [
-      { "type": "Feature", "properties": { "id": "ak16994521", "mag": 2.3, "time": 1507425650893, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [-151.5129, 63.1016, 0.0] } },
-      { "type": "Feature", "properties": { "id": "ak16994519", "mag": 1.7, "time": 1507425289659, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [-150.4048, 63.1224, 105.5] } },
-      { "type": "Feature", "properties": { "id": "ak16994517", "mag": 1.6, "time": 1507424832518, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [-151.3597, 63.0781, 0.0] } },
-      { "type": "Feature", "properties": { "id": "ci38021336", "mag": 1.42, "time": 1507423898710, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [-118.497, 34.299667, 7.64] } },
-      { "type": "Feature", "properties": { "id": "us2000b2nn", "mag": 4.2, "time": 1507422626990, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [-87.6901, 12.0623, 46.41] } },
-      { "type": "Feature", "properties": { "id": "ak16994510", "mag": 1.6, "time": 1507422449194, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [-151.5053, 63.0719, 0.0] } },
-      { "type": "Feature", "properties": { "id": "us2000b2nb", "mag": 4.6, "time": 1507420784440, "felt": null, "tsunami": 0 }, "geometry": { "type": "Point", "coordinates": [-178.4576, -20.2873, 614.26] } }
+      {"type": "Feature", "geometry": {"type": "Point", "coordinates": [-71.47636413574219, 42.298135270693116]}, "properties": {"ActivityIndex": 0.16583399999999998, "DayPeriod": "2020-01-01", "Geography": "30233212032332022"}},
+      {"type": "Feature", "geometry": {"type": "Point", "coordinates": [-71.39396667480469, 42.315400802951714]}, "properties": {"ActivityIndex": 0.027041000000000003, "DayPeriod": "2020-01-02", "Geography": "30233212033303120"}},
+      {"type": "Feature", "geometry": {"type": "Point", "coordinates": [-71.45439147949219, 42.08344551677251]}, "properties": {"ActivityIndex": 0.03126, "DayPeriod": "2020-01-03", "Geography": "30233212233002220"}},
+      {"type": "Feature", "geometry": {"type": "Point", "coordinates": [-71.22367858886719, 42.219873273583865]}, "properties": {"ActivityIndex": 0.041421, "DayPeriod": "2020-01-04", "Geography": "30233212303103000"}}
     ]
-  }
+  };
 
-  const handleChange = function (event) {
-    setDate( event.target.value );
-  }
 
   return (
     <div>
-      <p>Day: {date}</p>
-      <input type="range" min="0" max="31" value={date} onChange={e => setDate(e.target.value)} step="1" />
-
       <ReactMapGL
         {...viewport}
         mapboxApiAccessToken="pk.eyJ1Ijoibm90YWtheSIsImEiOiJja2g2a24yamkwZmFwMzB1aWw3OG90NDZmIn0.1IYxmhdVDKYXnQBQZx6vPw"
@@ -55,9 +52,17 @@ const Geo = () => {
             paint={{
               'circle-radius': 10,
               'circle-color': '#007cbf'
-            }} />
+            }}
+            filter ={['==', 'DayPeriod', dateString]}
+          />
         </Source>
       </ReactMapGL>
+
+      <div id='console'>
+        <p>Day: {dateString}</p>
+        <input type="range" min={minTime} max={maxTime} value={date} onChange={handleInput} step="86400000" />
+
+      </div>
     </div>
   );
 }
