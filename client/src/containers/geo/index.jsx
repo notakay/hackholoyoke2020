a2fca9561d1e8850294112fe2b6e9b4b62ba0235
josephbima <bima.jenie@gmail.com>
2020-11-08 16:40:56 +0700

    Added data, css slider and min zoom

--- client/src/containers/geo/index.jsx
+++ client/src/containers/geo/index.jsx
@@ -1,18 +1,21 @@
 import * as React from 'react';
 import { useState } from 'react';
 import ReactMapGL, { Source, Layer } from 'react-map-gl';
+import amherst_june from '../../../src/data/3023320213-06.json'
+
 
 const Geo = () => {
   const [viewport, setViewport] = useState({
-    width: 1200,
-    height: 600,
+    width: '100vw',
+    height: '80vh',
     latitude: 42.3601,
     longitude: -71.0589,
-    zoom: 8
+    zoom: 8,
+    minZoom: 7,
   });
 
-  const minTimeString = "2020-01-01";
-  const maxTimeString = "2020-01-31";
+  const minTimeString = "2020-06-01";
+  const maxTimeString = "2020-06-31";
 
   const minTime = (new Date(minTimeString)).getTime();
   const maxTime = (new Date(maxTimeString)).getTime();
@@ -27,16 +30,19 @@ const Geo = () => {
     return setDate(e.target.value);
   }
 
-  const geoJSON = {
-    "type": "FeatureCollection", 
-    "features": [
-      {"type": "Feature", "geometry": {"type": "Point", "coordinates": [-71.47636413574219, 42.298135270693116]}, "properties": {"ActivityIndex": 0.16583399999999998, "DayPeriod": "2020-01-01", "Geography": "30233212032332022"}},
-      {"type": "Feature", "geometry": {"type": "Point", "coordinates": [-71.39396667480469, 42.315400802951714]}, "properties": {"ActivityIndex": 0.027041000000000003, "DayPeriod": "2020-01-02", "Geography": "30233212033303120"}},
-      {"type": "Feature", "geometry": {"type": "Point", "coordinates": [-71.45439147949219, 42.08344551677251]}, "properties": {"ActivityIndex": 0.03126, "DayPeriod": "2020-01-03", "Geography": "30233212233002220"}},
-      {"type": "Feature", "geometry": {"type": "Point", "coordinates": [-71.22367858886719, 42.219873273583865]}, "properties": {"ActivityIndex": 0.041421, "DayPeriod": "2020-01-04", "Geography": "30233212303103000"}}
-    ]
-  };
+  // const geoJSON = {
+  //   "type": "FeatureCollection", 
+  //   "features": [
+  //     {"type": "Feature", "geometry": {"type": "Point", "coordinates": [-71.47636413574219, 42.298135270693116]}, "properties": {"ActivityIndex": 0.16583399999999998, "DayPeriod": "2020-01-01", "Geography": "30233212032332022"}},
+  //     {"type": "Feature", "geometry": {"type": "Point", "coordinates": [-71.39396667480469, 42.315400802951714]}, "properties": {"ActivityIndex": 0.027041000000000003, "DayPeriod": "2020-01-02", "Geography": "30233212033303120"}},
+  //     {"type": "Feature", "geometry": {"type": "Point", "coordinates": [-71.45439147949219, 42.08344551677251]}, "properties": {"ActivityIndex": 0.03126, "DayPeriod": "2020-01-03", "Geography": "30233212233002220"}},
+  //     {"type": "Feature", "geometry": {"type": "Point", "coordinates": [-71.22367858886719, 42.219873273583865]}, "properties": {"ActivityIndex": 0.041421, "DayPeriod": "2020-01-04", "Geography": "30233212303103000"}}
+  //   ]
+  // };
 
+  const geoJSON = amherst_june
+
+  // console.log(geoJSON['features'][0])
 
   return (
     <div>
@@ -50,7 +56,7 @@ const Geo = () => {
             id="point"
             type="circle"
             paint={{
-              'circle-radius': 10,
+              'circle-radius': 4,
               'circle-color': '#007cbf'
             }}
             filter ={['==', 'DayPeriod', dateString]}
@@ -60,8 +66,7 @@ const Geo = () => {
 
       <div id='console'>
         <p>Day: {dateString}</p>
-        <input type="range" min={minTime} max={maxTime} value={date} onChange={handleInput} step="86400000" />
-
+        <input class="slider" type="range" min={minTime} max={maxTime} value={date} onChange={handleInput} step="86400000" />
       </div>
     </div>
   );
